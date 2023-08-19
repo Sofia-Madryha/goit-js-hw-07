3eacaaba80400b9a282828f0779aac8ff2e5fa6e
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-08-19 19:42:00 +0300

    add the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const itemsList = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description}) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image js-img"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`).join('')
+}
+itemsList.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+itemsList.addEventListener('click', handlerClick);
+
+function handlerClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return
+  }
+  const {source} = evt.target.dataset;
+  const item = galleryItems.find((item)=> item.original === source)
+  const instance = basicLightbox.create(`
+      <div class = "modal">
+        <img src="${item.original}" alt="${item.description}}" />
+        </div>
+  
+  `)
+      instance.show()
+}
+
+
+
